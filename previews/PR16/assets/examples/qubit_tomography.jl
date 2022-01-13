c710cd98d1c61ee104b89526b4f6baea8e6bb114
Documenter.jl <documenter@juliadocs.github.io>
2022-01-13 04:56:27 +0000

    build based on 7616e0e

---
+++ previews/PR16/assets/examples/qubit_tomography.jl
@@ -0,0 +1,90 @@
+using ProgressMeter, LinearAlgebra, Statistics
+using ComplexSPSA
+
+Nvars = 2
+Nruns = 10^4
+Niters = 10^2
+Nmeasures = 10^4
+
+# Generate a random Haar ket in dimension d
+function rand_qudit(T::DataType, d...)
+    ket = randn(T, d...)
+
+    # Return normalized
+    return ket / LinearAlgebra.norm(ket)
+end
+
+# Fidelity between 2 qudits in their (complex) natural form
+function fidelity(guess, state)
+    prod2 = abs2(state' * guess)
+    norms2 = sum(abs2, guess) * sum(abs2, state)
+    return prod2 / norms2
+end
+
+# Reference (target) state
+refstate = rand_qudit(ComplexF64, Nvars)
+
+# Theoretical functions
+infidelity(z) = 1 - fidelity(z, refstate)
+metric(z1, z2) = -0.5fidelity(z1, z2)
+
+# Experimental function to simulate
+f(z) = simulate_experiment(infidelity(z), Nmeasures)
+
+# Post-iteration normalization
+postprocess(z) = z / sqrt(sum(abs2, z))
+
+
+labels = ["SPSA", "CSPSA", "SPSA2", "CSPSA2", "SPSA_QN", "CSPSA_QN", "CSPSA_QN_scalar"]
+
+zacc = ones(ComplexF64, Nvars, Niters, Nruns, length(labels))
+@showprogress for run in 1:Nruns
+    guess = rand(ComplexF64, Nvars)
+    
+    zacc[:, :, run, 1] = SPSA_on_complex(f, guess, Niters, postprocess=postprocess)
+    zacc[:, :, run, 2] = CSPSA(f, guess, Niters, postprocess=postprocess)
+    zacc[:, :, run, 3] = SPSA2_on_complex(f, guess,Niters, postprocess=postprocess)
+    zacc[:, :, run, 4] = CSPSA2(f, guess, Niters, postprocess=postprocess)
+    zacc[:, :, run, 5] = SPSA_QN_on_complex(f, metric, guess, Niters, postprocess=postprocess)
+    zacc[:, :, run, 6] = CSPSA_QN(f, metric, guess, Niters, postprocess=postprocess)
+    zacc[:, :, run, 7] = CSPSA_QN_scalar(f, metric, guess, Niters, postprocess=postprocess)
+end
+
+# Calculate statistics
+using Statistics: mean, var
+
+# Apply theoretical fidelity over variables variables obtained
+fz = apply_along_dim(infidelity, zacc, dim = 1)
+
+# Mean and variance
+fmean = mean(fz, dims = 3)
+fvar  =  var(fz, dims = 3)
+
+# Get rid of singleton dimensions
+fmean = squeeze(fmean);
+fvar = squeeze(fvar);
+
+using Plots
+
+# Make plot
+p = plot(yscale = :log10,                  # Log scale on the y-axis
+         xscale = :log10,
+         xlabel = "Number of iterations",  # 
+         ylabel = "Infidelity",            #
+         legend = :outertopright,
+)
+
+# Add all optimizers to the plot
+for i in eachindex(labels)
+    plot!(p, 1:Niters, fmean[:, i],  # Plot mean lines
+          ribbon = fvar[:, i],       # Plot variance
+          label = labels[i],         # Show optimizer label
+    )
+end
+    
+display(p)
+
+# p = plot!(p, dpi = 300)
+# savefig(p, "tomography_Nruns$Nruns.png")
+
+
