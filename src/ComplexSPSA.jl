718cceb52efb1219bd8d3daf3fa928966e72853f
Jorge Gidi <jorgegidi@gmail.com>
2022-08-30 16:14:21 -0400

    add export scalar methods

--- src/ComplexSPSA.jl
+++ src/ComplexSPSA.jl
@@ -7,11 +7,14 @@ export PaperPlot
 
 # Optimzers
 export SPSA_on_complex, CSPSA                     # First order
+
 export SPSA2_on_complex, CSPSA2                   # Second order
-export MCSPSA2, CSPSA2_full, CSPSA2_scalar        # (Experimental) second order
+export SPSA2_scalar_on_complex, CSPSA2_scalar     # Scalar Second order
+export MCSPSA2, CSPSA2_full                       #
 
 export SPSA_QN_on_complex, CSPSA_QN               # Natural gradient
 export SPSA_QN_scalar_on_complex, CSPSA_QN_scalar # Scalar Natural gradient
+
 # Tools
 export squeeze, apply_along_dim, simulate_experiment
 
