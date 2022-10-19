7e3919c070788bc622d35f0b0e3b2c6e01acdbde
Jorge Gidi <jorgegidi@gmail.com>
2022-10-19 01:15:47 -0300

    fix real_problem_as_complex and opposite

--- src/include/tools.jl
+++ src/include/tools.jl
@@ -1,7 +1,7 @@
 function real_problem_as_complex(fun_re, guess_re::AbstractVector{T}) where T <: Real
 
     fun_cx(x) = fun_re(reinterpret(Complex{T}, x))
-    guess_cx = copy(reinterpret(Complex{T}, guess_re))
+    guess_cx = copy(reinterpret(T, guess_re))
 
     return fun_cx, guess_cx
 end
@@ -9,7 +9,7 @@ end
 
 function complex_problem_as_real(fun_cx, guess_cx::AbstractVector{Complex{T}}) where T
 
-    fun_re(x) = fun_cx(reinterpret(T, x))
+    fun_re(x) = fun_cx(reinterpret(Complex{T}, x))
     guess_re = copy(reinterpret(T, guess_cx))
 
     return fun_re, guess_re
