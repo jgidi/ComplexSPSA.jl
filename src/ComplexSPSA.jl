e75c495d40d69c114611eacd53010f221f5613fc
Jorge Gidi <jorgegidi@gmail.com>
2022-10-25 17:29:20 -0300

    reorder imports and exports

--- src/ComplexSPSA.jl
+++ src/ComplexSPSA.jl
@@ -1,28 +1,37 @@
 module ComplexSPSA
 
+# External dependencies
+using QuantumToolkit
+using LinearAlgebra, Statistics
+
 # # Exported
 # # Qiskit wrapper submodule
 # export Qiskit
 # export PaperPlot
 
-# Optimzers
-export SPSA_on_complex, CSPSA                     # First order
+# Optimzer exports
 
-export SPSA2_on_complex, CSPSA2                   # Second order
-export SPSA2_scalar_on_complex, CSPSA2_scalar     # Scalar Second order
-export CSPSA2_full                                # Second order full
-
-export SPSA_QN_on_complex, CSPSA_QN               # Natural gradient
-export SPSA_QN_scalar_on_complex, CSPSA_QN_scalar # Scalar Natural gradient
-export CSPSA_QN_full                              # Natural gradient full
-
-# External dependencies
-using QuantumToolkit
-using LinearAlgebra, Statistics
+# First order
 export SPSA                     # Real
+export SPSA_on_complex, CSPSA   # Complex
+
+# Second order
 export SPSA2                    # Real
+export SPSA2_on_complex, CSPSA2 # Complex
+# Scalar
+export SPSA2_scalar                           # Real
+export SPSA2_scalar_on_complex, CSPSA2_scalar # Complex
+# Full
+export CSPSA2_full              # Only valid for Complex args
+
+# Quantum Natural
 export SPSA_QN                                    # Real
+export SPSA_QN_on_complex, CSPSA_QN               # Complex
+# Scalar
 export SPSA_QN_scalar                             # Real
+export SPSA_QN_scalar_on_complex, CSPSA_QN_scalar # Complex
+# Full
+export CSPSA_QN_full                              # Only valid for complex args
 
 include("include/gains.jl")
 include("include/estimates.jl")
